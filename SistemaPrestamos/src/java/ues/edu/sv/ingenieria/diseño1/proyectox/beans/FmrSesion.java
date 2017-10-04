d2a4e4c4f0112600148713fde7e666a76eb72eda
estuardo <estuardo@tua>
2017-10-04 17:40:31 -0600

    Validacion de contraseñas :3

--- SistemaPrestamos/src/java/ues/edu/sv/ingenieria/diseño1/proyectox/beans/FmrSesion.java
+++ SistemaPrestamos/src/java/ues/edu/sv/ingenieria/diseño1/proyectox/beans/FmrSesion.java
@@ -6,9 +6,13 @@
 package ues.edu.sv.ingenieria.diseño1.proyectox.beans;
 
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.xml.rpc.encoding.Serializer;
+import ues.edu.sv.ingenieria.diseño1.proyectox.controladores.ControladorSesion;
+import ues.edu.sv.ingenieria.diseño1.proyectox.controladores.ErrorPrestamo;
 
 /**
  *
@@ -18,6 +22,35 @@ import javax.xml.rpc.encoding.Serializer;
 @ApplicationScoped
 public class FmrSesion implements Serializable {
     
+    private String user;
+    private String contraseña;
+    private ControladorSesion sesion = new ControladorSesion();
+
+    public void comprobarDatos() {
+        try {
+            boolean autorizado= sesion.verificar(user, contraseña);
+            
+          
+        } catch (ErrorPrestamo ex) {
+            Logger.getLogger(FmrSesion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
     
     
 }
---
+++ SistemaPrestamos/src/java/ues/edu/sv/ingenieria/diseño1/proyectox/controladores/ControladorSesion.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ues.edu.sv.ingenieria.diseño1.proyectox.controladores;
+
+import java.sql.ResultSet;
+import servicios.Encriptador;
+import ues.edu.sv.ingenieria.diseño1.proyectox.definiciones.Conexion;
+
+/**
+ *
+ * @author estuardo
+ */
+public class ControladorSesion {
+    
+  // metodo para comprar contraseñas la que se esta ingresando y la almacenda en la base de datos
+    public boolean verificar(String login, String claveUsuario) throws ErrorPrestamo {
+        boolean verificar = false;
+        String nombre = null;
+        String clave = null;
+        // encripta la contraseña ingresada en login para comparar con la contraseña ya encriptada en la bd
+        Encriptador encrip = new Encriptador();
+        ResultSet resultado;
+        String claveComprobador = encrip.encriptar(claveUsuario);
+// trae de la bd la contraseña encriptada almacenada 
+        try {
+
+            Conexion conexion = new Conexion();
+            resultado = conexion.getValores("SELECT * FROM usuario WHERE login='" + login + "';");
+
+            while (resultado.next()) {
+                clave = resultado.getString("clave");
+                System.out.println(clave);
+                nombre = resultado.getString("nombre");
+                System.out.println(nombre);
+            }
+
+        } catch (Exception e) {
+            throw new ErrorPrestamo("Error al Obtener", "ControladorSesion.verificar", "Error al obtener clave ");
+
+        }
+        System.out.println(claveComprobador+" comparando con"+clave);
+        // compara que las contraseñas sean iguales y pone la flag en true si es asi de lo contrario permance en false
+        if (claveComprobador.equals(clave) ) {
+            verificar = true;
+        }
+
+        System.out.print(verificar);
+        return verificar;
+    }  
+    
+    
+    
+}
