28f553c35e86e05057bf56f6862a9844783efe1a
erick <erick@localhost>
2017-10-09 16:09:27 -0600

    Mostrando Estado de Resultados!

---
+++ SistemaPrestamos/src/java/ues/edu/sv/ingenieria/diseño1/proyectox/controladores/ControladorResultado.java
@@ -0,0 +1,43 @@
+package ues.edu.sv.ingenieria.diseño1.proyectox.controladores;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import ues.edu.sv.ingenieria.diseño1.proyectox.definiciones.Conexion;
+import ues.edu.sv.ingenieria.diseño1.proyectox.definiciones.EstadoResultados;
+
+/**
+ *
+ * @author erick
+ */
+public class ControladorResultado {
+
+    //METODO PARA OBTENER LAS CUENTAS DEL ESTADO DE RESULTADOS
+    public List<EstadoResultados> obtener() {
+
+        List<EstadoResultados> cuentasResultados = new ArrayList<>();
+        Conexion conexion = new Conexion();
+        ResultSet resultado;
+
+        System.out.println("Estoy en el metodo Obtener Cuentas del Estado de Resultados!");
+
+        try {
+
+            resultado = conexion.getValores("SELECT * FROM estado_resultados");
+
+            while (resultado.next()) {
+
+                cuentasResultados.add(new EstadoResultados(resultado.getInt("id_cuenta"),resultado.getString("nombre_cuenta")));
+                
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ControladorBalance.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return cuentasResultados;
+    }
+
+}
